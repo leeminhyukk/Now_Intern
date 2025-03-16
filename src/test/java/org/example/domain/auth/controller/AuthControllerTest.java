246754218630b2ce6a4a6820f207be44ebc48317
lklll1212 <lklll1212@gmail.com>
2025-03-16 10:34:07 +0900

    [add] 로그인 테스트 코드 작성

--- src/test/java/org/example/domain/auth/controller/AuthControllerTest.java
+++ src/test/java/org/example/domain/auth/controller/AuthControllerTest.java
@@ -1,6 +1,8 @@
 package org.example.domain.auth.controller;
 
+import org.example.domain.auth.dto.request.LoginRequest;
 import org.example.domain.auth.dto.request.SignupRequest;
+import org.example.domain.auth.dto.response.LoginResponse;
 import org.example.domain.auth.dto.response.SignupResponse;
 import org.example.domain.auth.service.AuthService;
 import org.junit.jupiter.api.Test;
@@ -39,5 +41,26 @@ public class AuthControllerTest {
         // Then
         assertEquals("testnickname", response.getNickname());  // 반환된 응답이 예상한 메시지와 일치하는지 확인
         verify(authService).signup(signupRequest); // authService의 signup 메서드가 호출되었는지 검증
+    }
+
+    @Test
+    public void testLogin() {
+        // Given
+        String username = "testuser";
+        String password = "testpassword";
+        LoginRequest loginRequest = new LoginRequest(username, password);
+
+        // 로그인 응답을 Mocking
+        String expectedToken = "testToken";  // "Bearer "는 LoginResponse에서 자르기 때문에 그냥 'testToken'만 설정
+        LoginResponse loginResponse = new LoginResponse("Bearer " + expectedToken);
+
+        // authService의 login 메서드가 호출되면 Mocking된 응답을 반환하도록 설정
+        when(authService.login(loginRequest)).thenReturn(loginResponse);
+
+        // When
+        LoginResponse response = authController.login(loginRequest);
+
+        // Then
+        assertEquals(expectedToken, response.getToken());  // 반환된 토큰에서 "Bearer "가 잘라진 값이 'testToken'인지 확인
     }
 }
